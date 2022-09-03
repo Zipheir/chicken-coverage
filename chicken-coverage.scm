92e386197d2ff43476f05334d74fda5cc6c904fa
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2022-09-03 10:32:49 -0400

    Add type checks.

--- chicken-coverage.scm
+++ chicken-coverage.scm
@@ -1,11 +1,25 @@
 ;;;; This is free software.  See LICENSE for copyright info.
 
 (import (chicken base)
+        (chicken condition)
         (chicken io)
         (chicken irregex)
         (chicken process-context)
         (chicken type)
         (slib wt-tree))
+
+(define-syntax assert-type
+  (syntax-rules ()
+    ((assert-type loc expr)
+     (unless expr
+       (abort
+        (make-composite-condition
+         (make-property-condition 'exn
+          'location loc
+          'message "type check failed"
+          'arguments (list 'expr))
+         (make-property-condition 'type)
+         (make-property-condition 'assertion)))))))
 
 ;; Describes a test-group line.  May need refinement.
 (define test-group-pattern
@@ -20,7 +34,8 @@
 ;; present or has already been confirmed, show a warning.
 (: confirm-form ((struct wt-tree) string -> (struct wt-tree)))
 (define (confirm-form dict name)
-  (assert (string? name))
+  (assert-type 'confirm-form (wt-tree? dict))
+  (assert-type 'confirm-form (string? name))
   (let ((v (wt-tree/lookup dict name 'not-found)))
     (case v
       ((#t)
@@ -36,6 +51,7 @@
 
 (: count-unconfirmed ((struct wt-tree) -> integer))
 (define (count-unconfirmed dict)
+  (assert-type 'count-unconfirmed (wt-tree? dict))
   (wt-tree/fold (lambda (junk b n)
                   (if b n (+ n 1)))
                 0
@@ -43,6 +59,8 @@
 
 (: print-results ((struct wt-tree) integer -> undefined))
 (define (print-results dict count)
+  (assert-type 'print-results (wt-tree? dict))
+  (assert-type 'print-results (integer? count))
   (display "Results:\n\n")
   (wt-tree/for-each
    (lambda (name tested)
@@ -60,6 +78,8 @@
 ;; confirmed.
 (: check-tests ((list-of symbol) input-port -> boolean))
 (define (check-tests forms port)
+  (assert-type 'check-tests (or (pair? forms) (null? forms)))
+  (assert-type 'check-tests (input-port? port))
   (let lp ((line (read-line port))
            (dict (make-dict forms)))
     (cond ((eof-object? line)
@@ -75,6 +95,7 @@
 
 (: make-dict ((list-of symbol) -> (struct wt-tree)))
 (define (make-dict forms)
+  (assert-type 'make-dict (or (pair? forms) (null? forms)))
   (foldl (lambda (dict sym)
            (wt-tree/add dict (symbol->string sym) #f))
          (make-wt-tree string-wt-type)
@@ -84,6 +105,7 @@
 ;; try to extract the module's list of exported identifiers.
 (: read-exported-forms (input-port -> (list-of symbol)))
 (define (read-exported-forms port)
+  (assert-type 'read-exported-forms (input-port? port))
   (let ((sexp (read port)))
     (unless (and (pair? sexp) (eqv? (car sexp) 'module))
       (error "Failed to read module S-exp" sexp))
